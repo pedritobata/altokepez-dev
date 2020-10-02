fec148c1a436046aabddbfe2d1bd8bfb31b45466
pedro martinez <pedromartinez_goyo@hotmail.com>
2020-10-02 00:51:56 -0500

    Testimonials Corusel logic , page switchers first part

--- altokepez-app/src/components/Testimonials/Testimonials.js
+++ altokepez-app/src/components/Testimonials/Testimonials.js
@@ -15,12 +15,14 @@ const lastSmallestBreakpoint = Number.parseInt(
 const Testimonials = (props) => {
   const [page, setPage] = useState(1);
   const [testimoniesPerPage, setTestimoniesPerPage] = useState(props.perpage);
+  const [pages, setPages] = useState(Array(Math.ceil(props.comments.length / testimoniesPerPage)).fill(''));
 
   useEffect(() => {
     const resizeWindowListener = (event) => {
       let vwWidth = event.target.visualViewport.width;
       if (vwWidth >= userIconDisapearMobileBreakpoint) {
         setTestimoniesPerPage(props.perpage);
+        setPages(Array(Math.ceil(props.comments.length / testimoniesPerPage)).fill(''));
       } else if (vwWidth >= lastSmallestBreakpoint) {
         setTestimoniesPerPage(2);
       } else {
@@ -32,34 +34,43 @@ const Testimonials = (props) => {
     return () => {
       window.removeEventListener("resize", resizeWindowListener);
     };
-  }, [testimoniesPerPage, props.perpage]);
+  }, [testimoniesPerPage, props.perpage,props.comments]);
+
+
 
   return (
-    <div
-      className="testimonials"
-      style={{
-        width: `calc(${props.comments.length / testimoniesPerPage} * 100vw`,
-        
-      }}
-    >
-      {props.comments.map((item) => {
-        return (
-          <div
-            key={item.name}
-            className="testimony"
-            style={{ flexGrow: `${(1 / testimoniesPerPage).toFixed(1)}` }}
-          >
-            <img
-              src={item.photo}
-              alt={item.name}
-              className="testimony__photo"
-            />
-            <h4 className="testimony__name">{item.name}</h4>
-            <p className="testimony__comment">{item.comment}</p>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div
+        className="testimonials"
+        style={{
+          width: `calc(${props.comments.length / testimoniesPerPage} * 100vw`,
+        }}
+      >
+        {props.comments.map((item) => {
+          return (
+            <div
+              key={item.name}
+              className="testimony"
+              style={{ flexGrow: `${(1 / testimoniesPerPage).toFixed(1)}` }}
+            >
+              <img
+                src={item.photo}
+                alt={item.name}
+                className="testimony__photo"
+              />
+              <h4 className="testimony__name">{item.name}</h4>
+              <p className="testimony__comment">{item.comment}</p>
+            </div>
+          );
+        })}
+      </div>
+      <div className="testimonials__pageSwitchersContainer">
+        {
+          
+          pages.map((_,index) => <p key={index}>X</p>)
+        }
+      </div>
+    </>
   );
 };
 
