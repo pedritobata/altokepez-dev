b19e6d563e2f4060bd444400ab709c7c967090c3
pedro martinez <pedromartinez_goyo@hotmail.com>
2020-10-22 01:03:13 -0500

    First attempt animating services

--- altokepez-app/src/containers/Services/Services.js
+++ altokepez-app/src/containers/Services/Services.js
@@ -6,24 +6,34 @@ import motoImage from "../../assets/images/vespa.png";
 import storeImage from "../../assets/images/barbershop.png";
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
 
+import {Animated} from "react-animated-css";
+
 const Services = () => {
   return (
-    <section className="services">
-      <SectionTitle title="Nuestros Servicios" />
-      <div className="services__container">
-        <Service
-          img={standImage}
-          title="En la barra"
-          description="Disfruta tu almuerzo en nuestra hermosa barra de siempre y acomódate como puedas!"
-        />
-        <Service
-          img={motoImage}
-          title="Delivery"
-          description="Debido a la coyuntura actual, hemos implementado nuestro servicio de Delivery cumpliendo rigurosamente todos los protocolos sanitarios"
-        />
-        <Service img={storeImage} title="Recojo en tienda" description="También puedes hacer tu pedido y decidir en qué momento pasarás a recogerlo a nuestro local" />
-      </div>
-    </section>
+    
+      <section className="services">
+        <SectionTitle title="Nuestros Servicios" />
+        <Animated
+    animationIn="fadeInUp"
+    animationInDuration={600}
+      animationInDelay={300}
+    
+    >
+        <div className="services__container">
+          <Service
+            img={standImage}
+            title="En la barra"
+            description="Disfruta tu almuerzo en nuestra hermosa barra de siempre y acomódate como puedas!"
+          />
+          <Service
+            img={motoImage}
+            title="Delivery"
+            description="Debido a la coyuntura actual, hemos implementado nuestro servicio de Delivery cumpliendo rigurosamente todos los protocolos sanitarios"
+          />
+          <Service img={storeImage} title="Recojo en tienda" description="También puedes hacer tu pedido y decidir en qué momento pasarás a recogerlo a nuestro local" />
+        </div>
+        </Animated>
+      </section>
   );
 };
 
--- altokepez-app/src/pages/Home/Home.js
+++ altokepez-app/src/pages/Home/Home.js
@@ -78,10 +78,10 @@ class Home extends React.Component{
                 <div className="home__container">
                     <SectionTitle title="Hoy te recomiendo" />
                     <div className="recommended">
-                        <RecommendedMeal title="Cachetitos de Róbalo" image="https://scontent.flim13-1.fna.fbcdn.net/v/t31.0-8/55504_418774461526827_2011798023_o.jpg?_nc_cat=100&_nc_sid=e007fa&_nc_eui2=AeFxI-qHohANka806_ESfDl8VlIEjGJaB59WUgSMYloHn6P1yTL0JbD-TzqVfZjLEYU&_nc_ohc=Lx3xMY83cuYAX-l9aUU&_nc_ht=scontent.flim13-1.fna&oh=584a311ead4194382fb3340fb3d2aa68&oe=5F8FA47A" />
+                        <RecommendedMeal title="Cachetitos de Róbalo" image="https://scontent.flim9-1.fna.fbcdn.net/v/t31.0-8/54079_418776008193339_1635302210_o.jpg?_nc_cat=105&ccb=2&_nc_sid=e007fa&_nc_ohc=XBbTC0hjjasAX_qAtb-&_nc_ht=scontent.flim9-1.fna&oh=59867def729c2a8f15c31c0e46626916&oe=5FB60E0D" />
                         <RecommendedMeal title="Tallarines salteados" image="https://scontent.flim9-1.fna.fbcdn.net/v/t1.0-9/80308779_3309508445786733_2992348077840400384_o.jpg?_nc_cat=103&_nc_sid=8024bb&_nc_ohc=NQCuBTBA5MsAX-UTJuh&_nc_ht=scontent.flim9-1.fna&oh=e3071e79e0f01476f8ce520cf1cf3a1f&oe=5F9D29F4" />
                         <RecommendedMeal  title="Combinado de Calamar" image="https://scontent.flim9-1.fna.fbcdn.net/v/t31.0-8/461703_285899748147633_33065499_o.jpg?_nc_cat=101&_nc_sid=7aed08&_nc_ohc=S57X7po5Q1kAX8RbZI8&_nc_ht=scontent.flim9-1.fna&oh=c62eb38c3643365a8fd1771b9c1077a6&oe=5F9ED01D"/>
-                        <RecommendedMeal  title="Saltadito con hongos" image="https://scontent.flim13-1.fna.fbcdn.net/v/t31.0-8/329160_418773264860280_2016262609_o.jpg?_nc_cat=110&_nc_sid=e007fa&_nc_eui2=AeGjNYEE6gT_dce5_wt9jpRv7028AT2PYr_vTbwBPY9ivx9bQJimuQMih5SblI4zlKc&_nc_ohc=fcIgniADsIAAX8dMeIe&_nc_ht=scontent.flim13-1.fna&oh=14c7b0d573c7ef0cc16f31fb35fc2e9f&oe=5F8FD7C0"/>
+                        <RecommendedMeal  title="Leche de Tigre" image="https://scontent.flim9-1.fna.fbcdn.net/v/t1.0-9/79908155_3319135381490706_6780833648972136448_o.jpg?_nc_cat=103&ccb=2&_nc_sid=8024bb&_nc_ohc=SOZDw-87VMUAX97wjyb&_nc_ht=scontent.flim9-1.fna&oh=f9dea9634bed366d9a63c3eef9a68e70&oe=5FB694D7"/>
                     </div>
                     <SectionTitle title="Algunos clásicos y ofertas" />
                     <div className="home__clasicos">
