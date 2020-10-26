cf1cd052310599a2dfc133fbdd92137b8c56252a
pedro martinez <pedromartinez_goyo@hotmail.com>
2020-10-25 21:05:46 -0500

    mask filter and scale effect issue when hovering over recommended meal inner text finally fixed

--- altokepez-app/src/components/RecommendedMeal/RecommendedMeal.js
+++ altokepez-app/src/components/RecommendedMeal/RecommendedMeal.js
@@ -4,40 +4,44 @@ import Button from "../UI/Button/Button";
 import { Controller, Scene } from "react-scrollmagic";
 
 const RecommendedMeal = (props) => {
-
   const calculateStyleOddEven = () => {
-    let resultStyle = {}
-    if(Number(props.id.replace("recommended", '')) % 2 === 0){
+    let resultStyle = {};
+    if (Number(props.id.replace("recommended", "")) % 2 === 0) {
       resultStyle = {
         animateStyle: "recommended-meal--fadeInRight",
-        offsetStyle: "recommended-meal--offsetRight"
-      } ;
-    }else{
+        offsetStyle: "recommended-meal--offsetRight",
+      };
+    } else {
       resultStyle = {
         animateStyle: "recommended-meal--fadeInLeft",
-        offsetStyle: "recommended-meal--offsetLeft"
-      } ;
+        offsetStyle: "recommended-meal--offsetLeft",
+      };
     }
     return resultStyle;
-  }
+  };
 
   return (
     <Controller>
-      <Scene duration={0} triggerElement={'#' + props.id}
-         classToggle={calculateStyleOddEven().animateStyle}
-         reverse={false}
-         triggerHook="onCenter">
-        <div className={`recommended-meal ${calculateStyleOddEven().offsetStyle}`} id={props.id}>
-            <div
-              className="recommended-meal__container"
-              style={{ backgroundImage: `url('${props.image}')` }}
-            >
-              
-            </div>
+      <Scene
+        duration={0}
+        triggerElement={"#" + props.id}
+        classToggle={calculateStyleOddEven().animateStyle}
+        reverse={false}
+        triggerHook="onCenter"
+      >
+        <div
+          className={`recommended-meal ${calculateStyleOddEven().offsetStyle}`}
+          id={props.id}
+        >
             <div className="recommended-meal__shop">
             <h3 className="recommended-meal__title">{props.title}</h3>
             <Button light value="ver plato" />
           </div>
+          <div
+            className="recommended-meal__container"
+            style={{ backgroundImage: `url('${props.image}')` }}
+          ></div>
+        
         </div>
       </Scene>
     </Controller>
