bb735656fb67441db4c8087de998f7f8e592ad8f
pedro martinez <pedromartinez_goyo@hotmail.com>
2020-10-30 01:45:24 -0500

    Dropdown almost completed

--- altokepez-app/src/pages/ShopScreen.js
+++ altokepez-app/src/pages/ShopScreen.js
@@ -1,29 +1,74 @@
-import React, {useState} from 'react';
-import './ShopScreen.scss';
+import React, { useState, useEffect, useRef } from "react";
+import "./ShopScreen.scss";
 
-const ShopScreen = props => {
+const ShopScreen = (props) => {
+  const [categories, setCategories] = useState([
+    "Ofertas",
+    "Clásicos",
+    "Combos",
+    "A la Carta",
+    "Bebidas",
+    "Especiales",
+  ]);
+  const [categorySelected, setCategorySelected] = useState(categories[0]);
+  const [showDropdownCategories, setShowDropdownCategories] = useState(false);
+  const [customDropdownStyle, setCustomDropdownStyle] = useState({});
 
-    const [categories, setCategories] = useState(["Ofertas","Clásicos", "Combos", "A la Carta", "Bebidas", "Especiales"]);
-    const [categorySelected, setCategorySelected] = useState(categories[0]);
+  const dropdownRef = useRef(null);
 
-    return (
-        <section className="shopscreen">
-            <div className="shopscreen__banner">
-                <h2>Qué comemos hoy?</h2>
-            </div>
-            <div className="shopscreen__dropdown">
-                <h4>{categorySelected}</h4><i class="fas fa-caret-down"></i>
-                <ul className="shopscreen__dropdownItems">
-                    {
-                        categories.filter(item => item !== categorySelected).map(item => {
-                            return <li key={item} className="shopscreen__dropdownItem">{item}</li>
-                        })
-                    }
-                </ul>
-            </div>
-            <div className="shopscreen__products"></div>
-        </section>
-    );
-}
+  useEffect(() => {
+    console.log("dropdownRef", dropdownRef);
+    console.log("client", dropdownRef.current.clientHeight);
+    if (showDropdownCategories) {
+      setCustomDropdownStyle({
+        // transform: `translateY(
+        // }px)`,
+        opacity: 1,
+        transform: `scaleY(1)`,
+        top: dropdownRef.current.parentElement.clientHeight
+      });
+    } else {
+      setCustomDropdownStyle({
+        // display: "none",
+        opacity: 0,
+        transform: `scaleY(0)`,
+        top: dropdownRef.current.parentElement.clientHeight
+      });
+    }
+  }, [showDropdownCategories]);
+
+  return (
+    <section className="shopscreen">
+      <div className="shopscreen__banner">
+        <h2>Qué comemos hoy?</h2>
+      </div>
+      <div
+        className="shopscreen__dropdown"
+        onClick={(e) => {
+            setShowDropdownCategories((prev) => !prev);
+        }}
+      >
+        <h4>{categorySelected}</h4>
+        <i class="fas fa-caret-down"></i>
+        <ul
+          ref={dropdownRef}
+          className="shopscreen__dropdownItems"
+          style={{...customDropdownStyle, width: '100%'}}
+        >
+          {categories
+            .filter((item) => item !== categorySelected)
+            .map((item) => {
+              return (
+                <li key={item} className="shopscreen__dropdownItem" onClick={(e) => setCategorySelected(e.target.innerText)}>
+                 {item}
+                </li>
+              );
+            })}
+        </ul>
+      </div>
+      <div className="shopscreen__products"></div>
+    </section>
+  );
+};
 
 export default ShopScreen;
